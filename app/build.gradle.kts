a38ebe551d69e5b2ce66997c9f4853c0a14b0ac7
MrDemonc <mrdemonlich@gmail.com>
2026-06-30 14:11:04 -0500

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.demonlab.lune"
         minSdk = 24
         targetSdk = 36
-        versionCode = 8
-        versionName = "1.5.0"
+        versionCode = 9
+        versionName = "1.5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
